bc694a2a21ae474f6b67e252412e1acedef5439d
Matheus Silva <matheussilva.fga@gmail.com>
2023-03-09 12:17:22 -0300

    Creating history object to allow navigantion from outside of components

---
+++ src/utils/history.ts
@@ -0,0 +1,11 @@
+import { NavigateFunction, Location } from 'react-router-dom';
+
+interface History {
+  navigate: NavigateFunction | null
+  location: Location | null
+}
+
+export const history: History = {
+  navigate: null,
+  location: null
+};
