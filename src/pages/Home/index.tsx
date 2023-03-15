d7b1b9c044775c7f03b526e84497aa7b64f6ac7c
Matheus Silva <matheussilva.fga@gmail.com>
2023-03-15 20:17:48 -0300

    Adding a navbar to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,24 @@
+import useAuthContext from '../../hooks/useAuthContext';
+
 import { Header } from '../../components/Header';
 import { Orders } from '../../components/Orders';
+import SecondaryButton from '../../components/SecondaryButton';
+import logOffIcon from '../../assets/images/log-off-icon.svg';
+
+import { Navbar } from './styles';
 
 export default function Home() {
+  const { handleLogout } = useAuthContext();
+
   return (
     <>
       <Header />
+      <Navbar>
+        <SecondaryButton onClick={handleLogout}>
+          <img src={logOffIcon} alt="ícone de log off" />
+          Sair
+        </SecondaryButton>
+      </Navbar>
       <Orders />
     </>
   );
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Navbar = styled.nav`
+  max-width: 1216px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin: 0 auto;
+`;
